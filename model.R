0fbb1055be4385ec957a8e59f63f6ee49bf44245
Changlin Li <13660416lin@163.com>
2022-01-11 16:31:11 +0800

    Add files via upload

--- model.R
+++ model.R
@@ -1,62 +1,89 @@
     
-    library(tidyverse); library(ggpubr); library(scales);
+    library(tidyverse); library(lubridate); library(ggpubr); library(scales);
+    #' size and biomass calculation (equation parameters refer to Calibration_FSC.R)
+    #' 
+    #' @author Changlin Li 
+    #' 
+    #' @param FSC normalized to standard beads (0.97-µm)
+    #' @param Abu abudance (10^3 cells mL-1)
+    #'
+    #' @return Dia cell size (µm, equivalent spherical diameter, ESD)
+    #' @return Biomass  (μg C L-1)
+    #' 
+    FSC_Abu_Biomass <- function(FSC, Abu) {
+      Dia <- 10 ^ (log10(FSC) * 0.250378257 + 0.135092677)
+      Vol <- 4/3*pi*((Dia)/2)^3
+      Carbon  <- Vol * 280
+      Biomass <- Carbon * Abu / 1000
+      return(Biomass)
+    }
+    
+    # the average peak time was approximately 04:30 for Prochlorococcus in the SCS study
     df <- read_csv("SeaFlow/Data/result.csv") %>% 
       filter(cruise %in% c("SCS2019_M4","SCS2019_SEATS","SCS2019_K11") & para == "Abu_Pro") %>% 
       mutate(peak_time = phi/2/pi * 24) 
-    
     # Assume that cell division does not occur during the photoperiod,
     # cell division occurs only during the several hours of darkness
     (peak_time <- mean(df$peak_time)) # ~ 4.5
     
+    # the input data for starting the model at 00:00 were the calculated average cell numbers and biomass
+    Sampletime <- read_csv("Data/Sampletime.csv") %>% 
+      mutate(Time = ymd_hms(paste(Date0, Time0))) %>%
+      select(Id, Time)
+
+    Pico <- read_csv("Data/Picoauto.csv") %>% 
+      left_join(Sampletime, by = "Id") %>% 
+      filter(Id %in% c("S13", "S29", "M5", "M21", "K5")) %>%
+      mutate(Pro_Biomass = FSC_Abu_Biomass(ProFSC, Pro)) 
+    median(Pico$Pro); median(Pico$Pro_Biomass);
+
+# Start the model ---------------------------------------------------------
 
     #' @param cn cell numbers (10^3 cells mL-1)
     #' @param b  biomass
-    #' @param cc is carbon per cell
-    #' @param g  grazing rate per half an hour
-    #' @param p  net photosynthetic rate per half an hour
-    #' @param r  respiration rate in dark per half an hour
-    #' @param d  rate of cell division per half an hour
-    p <- 0.027073685
-    g <- 0.0109559
-    r <- 0.00467002
-    d <- 0.02549995
-    d <- c(rep(d, 9), rep(0,27), rep(d, 13)); # cells divide according to this schedule, peak_time = 4.5
+    #' @param cs cell size
+    #' @param cc carbon per cell
+    #' @param g  grazing rate per hour
+    #' @param p  net primary production rate per hour
+    #' @param r  respiration rate in dark per hour
+    #' @param d  rate of cell division per hour
+    g <- 0.01821136 # Grazing occurs at a constant rate throughout the period of 24 hours.
+    p <- 0.048778
+    r <- 0.01227758
+    d <- 1/24
+    # The simulation was conducted on 5-minute time steps for 24 h.
+    p <- c(rep(0, 6*12), rep(p, 12*12), rep(0, 6*12)); # Net primary production occurs at a constant rate during the day.
+    r <- c(rep(r, 6*12), rep(0, 12*12), rep(r, 6*12)); # Respiration occurs at a constant rate throughout the night.
+    d <- c(rep(d, 4.5*12), rep(0, 13.5*12), rep(d, 6*12)); # cells divide according to this schedule, peak_time = 4.5
     
-    cn <- vector(mode = "numeric", length = 0); cn[1] <- 165
-    cc <- vector(mode = "numeric", length = 0); cc[1] <- 0.0149
-    b  <- vector(mode = "numeric", length = 0); b[1]  <- cn[1] * cc[1]
-
-    for (k in 2:13){ # time from t0.5 to t6.0
+    b0 <- vector(mode = "numeric", length = 0);
+    cn <- b0
+    cc <- b0
+    b  <- b0
+    # the input data for starting the model at 00:00
+    cn[1] <- 178
+    b[1]  <- 2.46
+    cc[1] <- b[1] / cn[1]
+    
+    for (k in 2:289){
       #k=2
-      cn[k] = cn[k-1] * (1 - g) * (1 + d[k-1])
-      b[k]  = b[k-1]  * (1 - r) * (1 - g)
+      cn[k] = cn[k-1] * (1 + d[k-1]/12 - g/12)
+      b[k]  = b[k-1]  * (1 + p[k-1]/12 - r[k-1]/12 - g/12)
       cc[k] = b[k]/cn[k]
     }
     
-    # cells divide during darkness, time from t6.0 to t18.0
-    for (k in 14:37){
-      cn[k] = cn[k-1] * (1 - g) * (1 + d[k-1])
-      b[k]  = b[k-1]  * (1 + p) * (1 - g)
-      cc[k] = b[k]/cn[k]
-    }
-    
-    for (k in 38:49){ # time from t18.0 to t24.0
-      cn[k] = cn[k-1] * (1 - g) * (1 + d[k-1])
-      b[k]  = b[k-1]  * (1 - r) * (1 - g)
-      cc[k] = b[k]/cn[k]
-    }
-
     cs = (cc * 1000 / 280 * 3 / 4 / pi) ^ (1/3) * 2 # cell size
-    cn[49]/cn[1];
-    cc[49]/cc[1];
-    b[49]/b[1];
-    max(cn);min(cn);
+    
+    cn[289]/cn[1];
+    cc[289]/cc[1];
+    b[289]/b[1];
+    max(cn);min(cn);max(cn)-min(cn);
     max(cs);min(cs);
     max(b) ;min(b);
     
     # draw plots
-    dat1 <- tibble(t = seq(0,24,.5),
-                   k = t * 2 + 1,
+    dat1 <- tibble(t = seq(0,24,1/12),
+                   k = t * 12 + 1,
                    b  = b,
                    cc = cc,
                    cn = cn,
@@ -149,12 +176,12 @@
     library(grid);  library(gtable)
     lwd_pt <- 1/(.pt*72.27/96)
     theme_set(theme_bw(base_size = 10, base_line_size = .7*lwd_pt, base_rect_size = .7*lwd_pt))
-    
+
     Theme1 <- theme(axis.text.y.left   = element_text(color = "#1fb050"),
                     axis.ticks.y.left  = element_line(color = "#1fb050"),
                     axis.line.y.left   = element_line(color = "#1fb050"),
                     axis.title.y.left  = element_text(color = "#1fb050", size = rel(0.9)),
-                    
+
                     axis.text.y.right  = element_text(color = "#ee7e32"),
                     axis.ticks.y.right = element_line(color = "#ee7e32"),
                     axis.line.y.right  = element_line(color = "#ee7e32"),
@@ -185,65 +212,57 @@
     plot_1a <- ggplot(dat1, aes(x = t)) +
       annotate("rect", xmin = -Inf, xmax = 6, ymin = -Inf, ymax = Inf, alpha = .4, fill = "gray") +
       annotate("rect", xmin = 18, xmax = Inf, ymin = -Inf, ymax = Inf, alpha = .4, fill = "gray") +
-      geom_line(aes(y = cn, color = "y1"), size = .5*lwd_pt) +
-      geom_point(aes(y = cn, color = "y1"), size = .7) +
+      geom_line(aes(y = cn, color = "y1"), size = lwd_pt) +
+      #geom_point(aes(y = cn, color = "y1"), size = .7) +
       scale_y_continuous(sec.axis = sec_axis(trans = ~ .* k2 + b2, name = "Cell size (mean ESD, µm)\n",
-                                             breaks = c(.43,.46,.49,.52)),
-                         breaks = breaks_pretty(5)) +
-      geom_line( aes(y = (cs - b2) / k2, color = "y2"), size = .5*lwd_pt) +
-      geom_point(aes(y = (cs - b2) / k2, color = "y2"), size = .7) +
-      geom_line( aes(y = (b  - b3) / k3, color = "y3"), size = .5*lwd_pt) +
-      geom_point(aes(y = (b  - b3) / k3, color = "y3"), size = .7) +
+                                             breaks = c(.42,.45,.48,.51)),
+                         breaks = breaks_pretty(4)) +
+      geom_line( aes(y = (cs - b2) / k2, color = "y2"), size = lwd_pt) +
+      #geom_point(aes(y = (cs - b2) / k2, color = "y2"), size = .7) +
+      geom_line( aes(y = (b  - b3) / k3, color = "y3"), size = lwd_pt) +
+      #geom_point(aes(y = (b  - b3) / k3, color = "y3"), size = .7) +
       annotate(geom='segment',y=-Inf,yend=-Inf,x=as.POSIXct(-Inf),xend=as.POSIXct(Inf), size = .7*lwd_pt) +
       annotate(geom='segment',y= Inf,yend= Inf,x=as.POSIXct(-Inf),xend=as.POSIXct(Inf), size = .7*lwd_pt) +
       scale_x_continuous(breaks = c(0,2,4,6,8,10,12,14,16,18,20,22,24), expand = expansion(mult = .03)) +
       scale_color_manual(values = c("y1" = "#1fb050", "y2" = "#ee7e32", "y3" = "#6f3996")) +
       guides(colour = "none") +
       labs(x = NULL, y = "Abundance (103 cells mL-1)", title = "Constant grazing") +
-      Theme1; plot_1a
+      Theme1; #plot_1a
     
     plot_1b <- ggplot(dat1, aes(x = t, y = b)) +
       geom_point() +
-      scale_y_continuous(breaks = c(2.1,2.4,2.7,3)) +
+      scale_y_continuous(breaks = breaks_pretty(4)) +
       xlab(NULL) + ylab("Biomass (µg C L-1)") +
-      Theme2; #plot_1b
+      Theme2; # plot_1b
     g1 <- plus_y_axis(plot_1a, plot_1b)
     #grid.newpage(); grid.draw(g1)
     
 # Grazing rate = 0 --------------------------------------------------------
     g <- 0
     
-    cn_ <- vector(mode = "numeric", length = 0); cn_[1] <- 165
-    cc_ <- vector(mode = "numeric", length = 0); cc_[1] <- 0.0149
-    b_  <- vector(mode = "numeric", length = 0); b_[1]  <- cn_[1] * cc_[1]
+    cn2 <- b0
+    cc2 <- b0
+    b2  <- b0
+    # the input data for starting the model at 00:00
+    cn2[1] <- 178
+    b2[1]  <- 2.46
+    cc2[1] <- b2[1] / cn2[1]
     
-    for (k in 2:13){ # time from t0.5 to t6.0
+    for (k in 2:289){
       #k=2
-      cn_[k] = cn_[k-1] * (1 - g) * (1 + d[k-1])
-      b_[k]  = b_[k-1]  * (1 - r) * (1 - g)
-      cc_[k] = b_[k]/cn_[k]
+      cn2[k] = cn2[k-1] * (1 + d[k-1]/12 - g/12)
+      b2[k]  = b2[k-1]  * (1 + p[k-1]/12 - r[k-1]/12 - g/12)
+      cc2[k] = b2[k]/cn2[k]
     }
     
-    # cells divide during darkness, time from t6.0 to t18.0
-    for (k in 14:37){
-      cn_[k] = cn_[k-1] * (1 - g) * (1 + d[k-1])
-      b_[k]  = b_[k-1]  * (1 + p) * (1 - g)
-      cc_[k] = b_[k]/cn_[k]
-    }
+    cs2 = (cc2 * 1000 / 280 * 3 / 4 / pi) ^ (1/3) * 2 # cell size
     
-    for (k in 38:49){ # time from t18.0 to t24.0
-      cn_[k] = cn_[k-1] * (1 - g) * (1 + d[k-1])
-      b_[k]  = b_[k-1]  * (1 - r) * (1 - g)
-      cc_[k] = b_[k]/cn_[k]
-    }
-    cs_ = (cc_ * 1000 / 280 * 3 / 4 / pi) ^ (1/3) * 2 # cell size
-    
-    dat2 <- tibble(t = seq(0,24,.5),
-                   k = t * 2 + 1,
-                   b  = b_,
-                   cc = cc_,
-                   cn = cn_,
-                   cs = cs_)
+    dat2 <- tibble(t = seq(0,24,1/12),
+                   k = t * 12 + 1,
+                   b  = b2,
+                   cc = cc2,
+                   cn = cn2,
+                   cs = cs2) 
     
     # Set up an equal scale combination plot - dual or three y-axis
     p1 <- ggplot(dat2, aes(x = t, y = cn)) + geom_point() + xlab(NULL) + guides(colour = "none");#p1
@@ -257,19 +276,18 @@
     k3 <- (range3[2] - range3[1]) / (range1[2] - range1[1])
     b3 <- (range1[2] * range3[1] - range1[1] * range3[2]) / (range1[2] - range1[1])
     
-    
     plot_2a <- ggplot(dat2, aes(x = t)) +
       annotate("rect", xmin = -Inf, xmax = 6, ymin = -Inf, ymax = Inf, alpha = .4, fill = "gray") +
       annotate("rect", xmin = 18, xmax = Inf, ymin = -Inf, ymax = Inf, alpha = .4, fill = "gray") +
-      geom_line(aes(y = cn, color = "y1"), size = .5*lwd_pt) +
-      geom_point(aes(y = cn, color = "y1"), pch = 15, size = .7) +
+      geom_line(aes(y = cn, color = "y1"), size = lwd_pt, linetype = 2) +
+      #geom_point(aes(y = cn, color = "y1"), pch = 15, size = .7) +
       scale_y_continuous(sec.axis = sec_axis(trans = ~ .* k2 + b2, name = "Cell size (mean ESD, µm)\n",
-                                             breaks = c(.43,.46,.49,.52)),
-                         breaks = breaks_extended(5)) +
-      geom_line( aes(y = (cs - b2) / k2, color = "y2"), size = .5*lwd_pt) +
-      geom_point(aes(y = (cs - b2) / k2, color = "y2"), pch = 15, size = .7) +
-      geom_line( aes(y = (b  - b3) / k3, color = "y3"), size = .5*lwd_pt) +
-      geom_point(aes(y = (b  - b3) / k3, color = "y3"), pch = 15, size = .7) +
+                                             breaks = c(.42,.45,.48,.51)),
+                         breaks = c(180,210,240,270)) +
+      geom_line( aes(y = (cs - b2) / k2, color = "y2"), size = lwd_pt, linetype = 2) +
+      #geom_point(aes(y = (cs - b2) / k2, color = "y2"), pch = 15, size = .7) +
+      geom_line( aes(y = (b  - b3) / k3, color = "y3"), size = lwd_pt, linetype = 2) +
+      #geom_point(aes(y = (b  - b3) / k3, color = "y3"), pch = 15, size = .7) +
       annotate(geom='segment',y=-Inf,yend=-Inf,x=as.POSIXct(-Inf),xend=as.POSIXct(Inf), size = .7*lwd_pt) +
       annotate(geom='segment',y= Inf,yend= Inf,x=as.POSIXct(-Inf),xend=as.POSIXct(Inf), size = .7*lwd_pt) +
       scale_x_continuous(breaks = c(0,2,4,6,8,10,12,14,16,18,20,22,24), expand = expansion(mult = .03)) +
@@ -290,7 +308,5 @@
     pdf(file = paste0("D:/Demo/1/", "plot_", nn, ".pdf"),  width = 3.74, height = 4.8)
     plot_grid(g1, g2, labels = c("a", "b"), align = 'h', ncol = 1)
     dev.off()
-    
-    
     
     
